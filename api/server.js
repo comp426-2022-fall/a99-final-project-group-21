45e637477616f09c7d7ee41c72a6cb02cd7dd98a
David Nam <69867708+Dnam00@users.noreply.github.com>
2022-12-07 21:28:36 -0500

    api authenticate and server.js

--- api/server.js
+++ api/server.js
@@ -1,18 +1,33 @@
 #!/usr/bin/env node
 
-import minimist from "minimist"
+//import minimist from "minimist"
 import express from "express"
+import passport from "passport"
 
 // This is where our server will be 
-//
+
+const passport = passport
+const session = require('express-session')
+const RedisStore = require('connect-redis') (session)
+
 const app = express();
-const args = minimist(process.argv.slice(2),{
-	default: {
-		port: 5000
-	},
-});
 
+app.use(session({
+	store: new RedisStore({
+	  url: config.redisStore.url
+	}),
+	secret: config.redisStore.secret,
+	resave: false,
+	saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
 
+// const args = minimist(process.argv.slice(2),{
+// 	default: {
+// 		port: 5000
+// 	},
+// });
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.json());
